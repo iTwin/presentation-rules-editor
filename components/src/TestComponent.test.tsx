f21b1422bd85d341f84f59cdd9f534ab96b538a6
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2021-05-04 14:28:05 +0300

    Switch to mocha test framework in components package

--- components/src/TestComponent.test.tsx
+++ components/src/TestComponent.test.tsx
@@ -3,12 +3,13 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
+import { expect } from "chai";
 import { render } from "@testing-library/react";
 import { TestComponent } from "./TestComponent";
 
 describe("TestComponent", () => {
   it("renders text", () => {
     const { getByText } = render(<TestComponent />);
-    expect(getByText("TestComponent")).not.toBeNull();
+    expect(getByText("TestComponent")).not.to.be.null;
   });
 });
