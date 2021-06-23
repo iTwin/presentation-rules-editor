8382c2e3566c30986ad8e8c4d5e979d93a905003
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2021-06-23 15:48:10 +0300

    Add hints for selecting an imodel (#30)

---
+++ app/frontend/src/app/utils/SelectIModelHint.tsx
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import * as React from "react";
+import { IModelApp } from "@bentley/imodeljs-frontend";
+import { VerticalStack } from "./VerticalStack";
+
+export function SelectIModelHint(): React.ReactElement {
+  return (
+    <VerticalStack>
+      <span>{IModelApp.i18n.translate("App:imodel-selector.no-imodel-selected")}</span>
+    </VerticalStack>
+  );
+}
