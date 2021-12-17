60a36a95f1785969830e8b13c052cb8b915bff49
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2021-12-17 17:55:12 +0200

    Fix iTwin.js breaking changes

--- app/frontend/src/app/ITwinJsApp/ui-framework/UIFramework.tsx
+++ app/frontend/src/app/ITwinJsApp/ui-framework/UIFramework.tsx
@@ -4,18 +4,28 @@
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
 import { Provider } from "react-redux";
-import { StateManager, ThemeManager } from "@itwin/appui-react";
+import { StateManager, ThemeManager, UiFramework, UiStateStorageHandler } from "@itwin/appui-react";
+import { MemoryUISettingsStorage } from "./MemoryUISettingsStorage";
 
 export interface UIFrameworkProps {
   children: React.ReactNode;
 }
 
 export function UIFramework(props: UIFrameworkProps): React.ReactElement {
+  React.useEffect(
+    () => {
+      void UiFramework.setUiStateStorage(new MemoryUISettingsStorage(), true);
+    },
+    [],
+  );
+
   return (
     <Provider store={StateManager.store}>
-      <ThemeManager>
-        {props.children}
-      </ThemeManager>
+      <UiStateStorageHandler>
+        <ThemeManager>
+          {props.children}
+        </ThemeManager>
+      </UiStateStorageHandler>
     </Provider>
   );
 }
