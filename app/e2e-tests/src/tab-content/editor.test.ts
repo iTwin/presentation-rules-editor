64a7489e7831bf84631a5b10e69e533cd0df657e
Robert Lukasonok <70327485+roluk@users.noreply.github.com>
2022-08-22 11:29:41 +0300

    Make end-to-end tests more reliable (#106)

--- app/e2e-tests/src/tab-content/editor.test.ts
+++ app/e2e-tests/src/tab-content/editor.test.ts
@@ -12,22 +12,20 @@ describe("editor #local", () => {
   });
 
   it("is populated with template ruleset", async () => {
-    const editor = await getEditor(page);
+    const editor = getEditor(page);
     expect(await editor.textContent()).to.contain('"Ruleset1"');
   });
 
   it("suggests completions based on ruleset schema", async () => {
-    const editor = await getEditor(page);
-
-    await (await editor.$("text=true"))!.dblclick();
+    const editor = getEditor(page);
+    await editor.locator("text=true").first().dblclick();
     await editor.press("Backspace");
     await editor.press("Control+Space");
-    const suggestions = await editor.waitForSelector(".suggest-widget");
-
-    const options = await suggestions.$$("[role=option]");
-    expect(options.length).to.be.equal(2);
-    expect(await options[0].$("text=false")).not.to.be.null;
-    expect(await options[1].$("text=true")).not.to.be.null;
+    const options = editor.locator(".suggest-widget");
+    await options.waitFor();
+    expect(await options.locator("[role=option]").count()).to.be.equal(2);
+    expect(await options.locator("[role=option]", { hasText: "false" }).elementHandle()).not.to.be.null;
+    expect(await options.locator("[role=option]", { hasText: "true" }).elementHandle()).not.to.be.null;
   });
 
   describe("ruleset submission", () => {
@@ -36,38 +34,38 @@ describe("editor #local", () => {
     });
 
     it("submits ruleset when button is clicked", async () => {
-      const editor = await getEditor(page);
-
-      await (await editor.$("text=Element"))!.dblclick();
+      const editor = getEditor(page);
+      await editor.locator("text=Element").dblclick();
       await editor.press("Backspace");
 
-      await (await editor.$("text=Submit ruleset"))!.click();
-      const treeWidget = await getWidget(page, "Tree");
-      await treeWidget.waitForSelector("text=The data required for this tree layout is not available in this iModel.");
+      await editor.locator("text=Submit ruleset").click();
+      await getWidget(page, "Tree")
+        .locator("text=The data required for this tree layout is not available in this iModel.")
+        .waitFor();
     });
 
     it("submits ruleset when keyboard shortcut is pressed", async () => {
-      const editor = await getEditor(page);
-
-      await (await editor.$("text=Element"))!.dblclick();
+      const editor = getEditor(page);
+      await editor.locator("text=Element").dblclick();
       await editor.press("Backspace");
 
       await editor.press("Alt+Enter");
-      const treeWidget = await getWidget(page, "Tree");
-      await treeWidget.waitForSelector("text=The data required for this tree layout is not available in this iModel.");
+      await getWidget(page, "Tree")
+        .locator("text=The data required for this tree layout is not available in this iModel.")
+        .waitFor();
     });
 
     it("submits ruleset when command is invoked from the command palette", async () => {
-      const editor = await getEditor(page);
-
-      await (await editor.$("text=Element"))!.dblclick();
+      const editor = getEditor(page);
+      await editor.locator("text=Element").dblclick();
       await editor.press("Backspace");
 
       await editor.press("F1");
       await editor.type("Submit ruleset");
       await editor.press("Enter");
-      const treeWidget = await getWidget(page, "Tree");
-      await treeWidget.waitForSelector("text=The data required for this tree layout is not available in this iModel.");
+      await getWidget(page, "Tree")
+        .locator("text=The data required for this tree layout is not available in this iModel.")
+        .waitFor();
     });
   });
 });
--- app/e2e-tests/src/widgets/tree.test.ts
+++ app/e2e-tests/src/widgets/tree.test.ts
@@ -11,18 +11,18 @@ describe("tree widget #local", () => {
   });
 
   it("displays tree hierarchy", async () => {
-    const treeWidget = await getWidget(page, "Tree");
-    await treeWidget.waitForSelector(".core-tree-node");
+    const treeWidget = getWidget(page, "Tree");
+    await treeWidget.locator(".core-tree-node").first().waitFor();
   });
 
   it("updates tree when ruleset changes", async () => {
-    const editor = await getEditor(page);
+    const editor = getEditor(page);
     await page.click('text=""rules""');
     await editor.press("Control+Enter");
     await editor.type('{ "ruleType": "CheckBox" },');
     await editor.press("Alt+Enter");
 
-    const treeWidget = await getWidget(page, "Tree");
-    await treeWidget.waitForSelector("input[type=checkbox]", { state: "attached" });
+    const treeWidget = getWidget(page, "Tree");
+    await treeWidget.locator("input[type=checkbox]").first().waitFor({ state: "attached" });
   });
 });
