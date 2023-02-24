3a7238af9da4bede58eae55f2e818dffc22aba24
Grigas <grigas.petraitis@bentley.com>
2023-02-24 14:00:04 +0200

    Start using `ECSchemaRpcInterface`

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -7,6 +7,7 @@ import { rpcInterfaces } from "@app/common";
 import { IModelHost, IModelHostConfiguration } from "@itwin/core-backend";
 import { Logger, LogLevel } from "@itwin/core-bentley";
 import { RpcConfiguration } from "@itwin/core-common";
+import { ECSchemaRpcImpl } from "@itwin/ecschema-rpcinterface-impl";
 import { BackendIModelsAccess } from "@itwin/imodels-access-backend";
 import { Presentation, PresentationBackendLoggerCategory, PresentationBackendNativeLoggerCategory } from "@itwin/presentation-backend";
 import { PresentationRulesEditorRpcImpl } from "./PresentationRulesEditorRpcImpl";
@@ -33,6 +34,7 @@ void (async () => {
   });
 
   RpcConfiguration.developmentMode = true;
+  ECSchemaRpcImpl.register();
   PresentationRulesEditorRpcImpl.register();
 
   await initialize(rpcInterfaces);
--- app/common/src/Rpcs.ts
+++ app/common/src/Rpcs.ts
@@ -3,10 +3,12 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { IModelReadRpcInterface, IModelTileRpcInterface, SnapshotIModelRpcInterface } from "@itwin/core-common";
+import { ECSchemaRpcInterface } from "@itwin/ecschema-rpcinterface-common";
 import { PresentationRpcInterface } from "@itwin/presentation-common";
 import { PresentationRulesEditorRpcInterface } from "./PresentationRulesEditorRpcInterface";
 
 export const rpcInterfaces = [
+  ECSchemaRpcInterface,
   IModelTileRpcInterface,
   IModelReadRpcInterface,
   SnapshotIModelRpcInterface,
