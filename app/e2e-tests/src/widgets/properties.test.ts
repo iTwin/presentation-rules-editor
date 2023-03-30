a01279f62d4e69f032dba564717ea6ebbb4795f2
Grigas <grigas.petraitis@bentley.com>
2023-03-30 16:45:30 +0300

    Add an error boundary to property grid widget

--- app/e2e-tests/src/widgets/properties.test.ts
+++ app/e2e-tests/src/widgets/properties.test.ts
@@ -34,6 +34,22 @@ describe("properties widget #local", () => {
     await propertiesWidget.locator("text=custom_category").waitFor();
   });
 
+  it("renders error status on error", async () => {
+    const propertiesWidget = getWidget(page, "Properties");
+    await propertiesWidget.locator("text=Select element(s) to view properties.").waitFor();
+
+    // simulate network error
+    await page.context().route(
+      (url) => {
+        return url.pathname.includes("getPagedContent");
+      },
+      async (route) => route.abort(),
+    );
+
+    await selectAnyTreeNode(page);
+    await propertiesWidget.locator(`text="Error"`).waitFor();
+  });
+
   // eslint-disable-next-line @typescript-eslint/no-shadow
   async function selectAnyTreeNode(page: Page): Promise<void> {
     const treeWidget = getWidget(page, "Tree");
