da5132e01c07cbace843c70a17a2de539f3b96d6
Grigas <35135765+grigasp@users.noreply.github.com>
2023-05-04 10:06:07 +0300

    Table errors out only on failure to load columns. Error loading rows results in no rows.

--- app/e2e-tests/src/widgets/properties.test.ts
+++ app/e2e-tests/src/widgets/properties.test.ts
@@ -42,6 +42,7 @@ describe("properties widget #local", () => {
     await propertiesWidget.locator("text=custom_category").waitFor();
   });
 
+  // TODO: unskip after upgrading itwinjs-code dependencies to 4.0
   it.skip("renders error status on error", async () => {
     const propertiesWidget = getWidget(page, "Properties");
     await propertiesWidget.locator("text=Select element(s) to view properties.").waitFor();
