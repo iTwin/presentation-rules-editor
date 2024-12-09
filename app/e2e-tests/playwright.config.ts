4c2c4d87319668631d478f3098bc1c3060de7688
JonasDov <100586436+JonasDov@users.noreply.github.com>
2024-12-09 12:46:26 +0200

    Update changelog and build script (#171)
    * Update changelog
    
    * Update build script
    
    * Add tests junit script
    
    * Make every e2e script use junit

--- app/e2e-tests/playwright.config.ts
+++ app/e2e-tests/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["list"], ["html", { open: "never" }]],
+  reporter: [["list"], ["html", { open: "never" }], ["junit", { outputFile: "junit-results.xml" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
